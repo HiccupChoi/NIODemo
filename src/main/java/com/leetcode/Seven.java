7478ed29be9fdbfd47a89572cbb8c80b3d69b912
蔚欢 <yuhuan07@meituan.com>
2019-08-20 18:06:18 +0800

    LeetCode 7.获取最大公共前缀

---
+++ src/main/java/com/leetcode/Seven.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @Author: Hiccup
+ * @Date: 2019/8/20 5:56 PM
+ * 获取最大公共前缀
+ */
+public class Seven {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+
+}
