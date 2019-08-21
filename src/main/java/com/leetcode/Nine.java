2c5d7e653b92f272e9028e279da35b379d3a8c19
蔚欢 <yuhuan07@meituan.com>
2019-08-21 17:47:38 +0800

    LeetCode 9.改进合并有序列表 10.删除排序数组中的重复项

--- src/main/java/com/leetcode/Nine.java
+++ src/main/java/com/leetcode/Nine.java
@@ -26,19 +26,7 @@ public class Nine {
         }
         ListNode mergeList = new ListNode(0);
         ListNode returnList = mergeList;
-        while (l1 != null || l2 != null){
-            if (l1 == null){
-                mergeList.next = new ListNode(l2.value);
-                mergeList = mergeList.next;
-                l2 = l2.next;
-                continue;
-            }
-            if (l2 == null){
-                mergeList.next = new ListNode(l1.value);
-                mergeList = mergeList.next;
-                l1 = l1.next;
-                continue;
-            }
+        while (l1 != null && l2 != null){
             if (l1.value < l2.value){
                 mergeList.next = new ListNode(l1.value);
                 mergeList = mergeList.next;
@@ -48,6 +36,12 @@ public class Nine {
                 mergeList = mergeList.next;
                 l2 = l2.next;
             }
+        }
+        if (l1 == null){
+            mergeList.next = l2;
+        }
+        if (l2 == null){
+            mergeList.next = l1;
         }
         return returnList.next;
     }
---
+++ src/main/java/com/leetcode/Ten.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+/**
+ * @Author: Hiccup
+ * @Date: 2019/8/21 5:38 PM
+ * 删除排序数组中的重复项
+ */
+public class Ten {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        return 0;
+    }
+
+}
